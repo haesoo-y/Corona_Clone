d80b7d8c69bb7cfe90601a2ed6bad376ef4223c6
Harry_Yoon <haesoo9410@gmail.com>
2020-12-13 03:18:40 +0900

    fire-works

--- main.js
+++ main.js
@@ -1,35 +1,70 @@
 (function(){
+
+    function FireAction(a) {
+        
+        if(a) {
+            for (let i = 0; i < FireWorks.length; i++) {
+                FireWorks[i].style.opacity = '1';
+                FireWorks[i].style.transition = `1s ${i}s`;
+            }
+        }else {
+            for (let i = 0; i < FireWorks.length; i++) {
+                FireWorks[i].style.transition = `0s 0s`;
+                FireWorks[i].style.opacity = '0';
+            }
+        }
+    }
+
     const textElems = document.querySelectorAll('.text-item');
     const graphicElems = document.querySelectorAll('.graphic-item');
+    const FireWorks = document.querySelectorAll('.fire-scene');
     let currentItem = graphicElems[0];
+    let ioIndex;
+
+    const io = new IntersectionObserver(function(entries, observer){
+        ioIndex = entries[0].target.dataset.index * 1;
+        // 관찰대상이 눈에 보일 때 실행
+    })
+
+    // for (let i = 0; i< FireWorks.length; i++) {
+        
+    // }
 
     for (let i = 0; i < textElems.length; i++) {
+        io.observe(textElems[i]);
+        // 관찰대상으로 등록
         textElems[i].dataset.index = i;
         graphicElems[i].dataset.index = i;
     }
 
-    function activated() {
+    function activated(action) {
         currentItem.classList.add('visible');
-
+        if (action) {
+            FireAction(true);
+        }
     }
 
-    function inactivated() {
+    function inactivated(action) {
         currentItem.classList.remove('visible');
+        if (action) {
+            FireAction(false);
+        }
     }
 
     window.addEventListener('scroll', function(){
         let textitem;
         let boundingRect;
 
-        for (let i = 0; i < textElems.length; i++){
+        for (let i = ioIndex - 1; i < ioIndex + 2; i++){
             textitem = textElems[i];
+            if (!textitem) continue;
             boundingRect = textitem.getBoundingClientRect();
             
             if (boundingRect.top > window.innerHeight * 0.1 && 
                 boundingRect.top < window.innerHeight * 0.8) {
-                    inactivated();
+                    inactivated(currentItem.dataset.action);
                     currentItem = graphicElems[textitem.dataset.index];
-                    activated();
+                    activated(currentItem.dataset.action);
                 }
 
         }
